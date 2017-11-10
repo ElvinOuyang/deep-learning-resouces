2226157b26ae87eb091151c84db54037cdbb1f09
Chuanye "Elvin" Ouyang <elvin.ouyang@gmail.com>
2017-11-10 08:44:07 -0500

    upload lua torch7 map training and pytorch map training scripts

--- PyTorch mlp training/mnist_mlp_training.py
+++ PyTorch mlp training/mnist_mlp_training.py
@@ -46,6 +46,7 @@ optimizer = optim.SGD(mlp.parameters(), lr=0.1, momentum=0.9)
 
 # define a training epoch function
 def trainEpoch(dataloader, epoch):
+    print("Training Epoch %i" % (epoch + 1))
     mlp.train()
     running_loss = 0
     for i, data in enumerate(dataloader, 0):
@@ -61,6 +62,43 @@ def trainEpoch(dataloader, epoch):
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 50))
             running_loss = 0.0
+"""
+def trainEpoch(dataloader, epoch):
+    print("Training Epoch %i" % (epoch + 1))
+    mlp.train()
+    for i, data in enumerate(dataloader, 0):
+        inputs, labels = data
+        inputs, labels = Variable(inputs), Variable(labels)
+        optimizer.zero_grad()
+        outputs = mlp(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+"""
+
+"""
+def validateModel(dataloader, epoch):
+    mlp.eval()
+    test_loss = 0
+    correct = 0
+    pred = np.array([])
+    targ = np.array([])
+    for inputs, targets in dataloader:
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = mlp(inputs)
+        test_loss += F.nll_loss(outputs, targets, size_average=False).data[0]
+        pred = np.append(pred, outputs.data.max(1)[1].numpy())
+        targ = np.append(targ, targets.data.numpy())
+        prd = outputs.data.max(1)[1]
+        correct += prd.eq(targets.data.view_as(prd)).sum()
+    test_loss /= len(dataloader.dataset)
+    test_acc = correct / len(dataloader.dataset)
+    cm = confusion_matrix(targ, pred)
+    print('[Epoch %i] Accuracy: %.2f, Average Loss: %.2f' %
+          (epoch, test_acc, test_loss))
+    print(cm)
+    return test_loss, test_acc, cm
+"""
 
 
 # define a testing function
@@ -78,6 +116,17 @@ def testModel(dataloader):
     print('Test set: Average loss: {:.3f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
           test_loss, correct, len(testloader.dataset),
           100. * correct / len(testloader.dataset)))
+"""
+def testModel(dataloader):
+    mlp.eval()
+    pred = np.array([])
+    for inputs, _ in dataloader:
+        inputs = Variable(inputs)
+        outputs = mlp(inputs)
+        pred = np.append(pred, outputs.data.max(
+            1, keepdim=True)[1].view(1, -1).numpy())
+    return pred
+"""
 
 
 # run the training epoch 30 times and test the result
@@ -85,6 +134,18 @@ for epoch in range(30):
     trainEpoch(trainloader, epoch)
 
 testModel(testloader)
+
+"""
+epoch_loss = []
+epoch_acc = []
+for epoch in range(100):
+    trainEpoch(trainloader, epoch)
+    loss, acc, _ = validateModel(valloader, epoch)
+    epoch_loss.append(loss)
+    epoch_acc.append(acc)
+
+pred = testModel(testloader)
+"""
 
 """DEBUGGING CODES
 # download one batch of data for input examination
